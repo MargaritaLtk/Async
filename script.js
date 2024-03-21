ee8c71143e5d1eb66077efc3830d7d5dd20fd922
MargaritaLtk <123299004+MargaritaLtk@users.noreply.github.com>
2024-03-21 03:31:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,47 @@ const whereAmI = function () {
 };
 
 btn.addEventListener('click', whereAmI);
+
+// Task 2
+
+const imgContainer = document.querySelector('.images');
+
+const wait = function (seconds) {
+   return new Promise(function (resolve) {
+      setTimeout(resolve, seconds * 1000);
+   });
+};
+
+const createImage = function (imgPath) {
+   return new Promise(function (resolve, reject) {
+      const img = document.createElement('img');
+      img.src = imgPath;
+
+      img.addEventListener('error', function () {
+         reject(new Error('Image not found'));
+      });
+
+      img.addEventListener('load', function () {
+         imgContainer.append(img);
+         resolve(img);
+      });
+   });
+};
+
+let currentImg;
+
+createImage('/img/img-1.jpg')
+   .then((img) => {
+      currentImg = img;
+      return wait(2);
+   })
+   .then(() => {
+      currentImg.style.display = 'none';
+      return createImage('/img/img-2.jpg');
+   })
+   .then((img) => {
+      currentImg = img;
+      return wait(2);
+   })
+   .then(() => (currentImg.style.display = 'none'))
+   .catch((err) => console.error(err));
